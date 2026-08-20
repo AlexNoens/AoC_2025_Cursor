c39de0b7f2a919a1402be37c4101bcca47b3d00d
AlexNoens <alex.j.noens@gmail.com>
2026-08-20 11:00:31 -0400

    Update day2.py

--- Day2/day2.py
+++ Day2/day2.py
@@ -31,6 +31,45 @@ def part1():
 
     print(f"Part 1: {total}")
 
+def check_mirrored(string):
+    length = len(string)
+    mid = length // 2
+    # odd lengths can't have repeated pairs
+    if (length % 2 != 0):
+        return False
+    # check the mirrored parts and compare. If they are the same, add to total
+    if (string[0:(mid)] == string[(mid):length]):
+        print(f"Mirrored match: {string}")
+        return True
+
+def check_all(string):
+    length = len(string)
+    mid = length // 2
+    # Even lengths have been checked by "mirrored", only check odd lengths here
+    if (length % 2 != 0):
+        for i in range(0, length):
+            if (string[i] != string[0]):
+                return False
+        return True
+    else:
+        return False
+
+# Half (mirrored) and all (odd length) have been checked, now check for repeated sequences of any length
+def check_patterns(string):
+    length = len(string)
+    patternFound = True
+    for i in range(2, length//2):
+        patternFound = True
+        if (length%i == 0):
+            pattern = string[0:i]
+            for j in range(0, length//i):
+                if (string[j*i:j*i+i] != pattern):
+                    patternFound = False
+                    break
+            if (patternFound):
+                print(f"Pattern match: {string} with pattern: {pattern}")
+                return patternFound
+    return False
 
 def part2():
     ids = open_file()
@@ -38,9 +77,17 @@ def part2():
     for id in ids:
         for i in range(atoi(id[0]), atoi(id[1])+1):
             string = str(i)
-            length = len(string)
+            if (len(string) == 1):
+                continue
+            # Mirrored, and Even all matched
+            if (check_mirrored(string)):
+                total += i
+            # Odd length all matched
+            elif (check_all(string)):
+                total += i
             # use recursion to check for repeated sequences of any length
-
+            elif (check_patterns(string)):
+                total += i
     print(f"Part 2: {total}")
 
 if __name__ == "__main__":
